5267325475ae2ecc67e719ab90e596b840f9033f
piet <spetakel123@gmail.com>
2021-12-03 12:56:07 +0100

    flex

---
+++ flex/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const fs = require("fs/promises");
+const cors = require("cors");
+const path = require("path");
+
+const makeCsvDir = async () => {
+    try { await fs.mkdir("csv"); } catch {}
+}
+
+const makeCsvFile = async (name, content) => {
+    try { await fs.writeFile("csv/" + name + ".csv", content); } catch {}
+}
+
+const appendCsvFile = async (name, content) => {
+    try { await fs.appendFile("csv/" + name + ".csv", "\n" + content); } catch {}
+}
+
+const formatFileFriendly = (date) => {
+  return `doc_${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+  + `_${date.getUTCHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+}
+
+const sanitizeBody = (body) => {
+    const neobody = {};
+    for (let i in body) {
+        neobody[i] = body[i].toString().replaceAll(",", "_")
+    }
+    return neobody;
+}
+
+export const flexRouter = async () => {
+    
+    const time = new Date();
+    const filename = formatFileFriendly(time);    
+    
+    await makeCsvDir();
+    await makeCsvFile(filename, "navn,dato,tjekket ind,tjekket ud");
+    
+    const app = express.Router();
+    app.use(cors(), express.json());
+
+    app.get("/download", (req, res) => {
+        res.sendFile(path.join(__dirname, `./csv/${filename}.csv`));
+    })
+
+    app.post("/add", (req, res) => {
+        const { name, date, begin, end } = sanitizeBody(req.body);
+        appendCsvFile(filename, `${name},${date},${begin},${end}`);
+        res.status(200).json({
+            message: "added to file"
+        })    
+    })
+
+    app.use('/', express.static("./public")); 
+}
---
+++ flex/public/script.js
@@ -0,0 +1,28 @@
+const onSubmit = async (name, date, begin, end) => {
+    if (name === "" || date === "" || begin === "" || end === "") {
+        alert("Missing required fields");
+        return;
+    }
+    
+    const headers = new Headers({"Content-Type": "application/json"});
+    const body = JSON.stringify({ name, date, begin, end });
+    const res = await (await fetch("/add", { 
+        headers,
+        body,
+        method: "POST"
+    })).json();
+
+    alert(res.message);
+}
+
+const main = () => {
+    const name   = document.getElementById("name");
+    const date   = document.getElementById("date");
+    const begin  = document.getElementById("begin");
+    const end    = document.getElementById("end");
+    const submit = document.getElementById("submit");
+    
+    submit.addEventListener("click", () => onSubmit(name.value, date.value, begin.value, end.value) ); 
+}
+
+main();
