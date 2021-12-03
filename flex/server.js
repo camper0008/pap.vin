30f92b3301147ca99bc1dd494d0e1eba982685c5
piet <42157469+camper0008@users.noreply.github.com>
2021-12-03 15:36:02 +0100

    quotes and port

--- flex/server.js
+++ flex/server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const fs = require("fs/promises");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const fs = require('fs/promises');
+const cors = require('cors');
+const path = require('path');
 
 const makeCsvDir = async () => {
-    try { await fs.mkdir("csv"); } catch {}
+    try { await fs.mkdir('csv'); } catch {}
 }
 
 const makeCsvFile = async (name, content) => {
-    try { await fs.writeFile("csv/" + name + ".csv", content); } catch {}
+    try { await fs.writeFile('csv/' + name + '.csv', content); } catch {}
 }
 
 const appendCsvFile = async (name, content) => {
-    try { await fs.appendFile("csv/" + name + ".csv", "\n" + content); } catch {}
+    try { await fs.appendFile('csv/' + name + '.csv', '\n' + content); } catch {}
 }
 
 const formatFileFriendly = (date) => {
@@ -23,7 +23,7 @@ const formatFileFriendly = (date) => {
 const sanitizeBody = (body) => {
     const neobody = {};
     for (let i in body) {
-        neobody[i] = body[i].toString().replaceAll(",", "_")
+        neobody[i] = body[i].toString().replaceAll(',', '_')
     }
     return neobody;
 }
@@ -34,26 +34,24 @@ const main = async () => {
     const filename = formatFileFriendly(time);    
     
     await makeCsvDir();
-    await makeCsvFile(filename, "navn,dato,tjekket ind,tjekket ud");
+    await makeCsvFile(filename, 'navn,dato,tjekket ind,tjekket ud');
     
     const app = express();
     app.use(cors(), express.json());
 
-    app.get("/download", (req, res) => {
+    app.get('/api/download', (req, res) => {
         res.sendFile(path.join(__dirname, `./csv/${filename}.csv`));
     })
 
-    app.post("/add", (req, res) => {
+    app.post('/api/add', (req, res) => {
         const { name, date, begin, end } = sanitizeBody(req.body);
         appendCsvFile(filename, `${name},${date},${begin},${end}`);
         res.status(200).json({
-            message: "added to file"
+            message: 'added to file'
         })    
     })
 
-    app.use('/', express.static("./public")); 
-
-    app.listen(8000);
+    app.listen(8001);
 }
 
 main();
